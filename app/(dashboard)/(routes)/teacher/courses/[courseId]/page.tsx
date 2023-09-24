b0e67f3c5d56f241596b93e5d53f7387bff5211b
timons-AI <123802219+timons-AI@users.noreply.github.com>
2023-09-24 16:23:12 +0300

    Course Category Form

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -6,6 +6,7 @@ import { redirect } from "next/navigation";
 import { TitleForm } from "./_components/title-form";
 import { DescriptionForm } from "./_components/description-form";
 import { ImageForm } from "./_components/image-form";
+import { CategoryForm } from "./_components/category-form";
 
 const CourseId = async ({ params }: {params: { courseId: string}}) => {
     const { userId } = auth();
@@ -15,6 +16,12 @@ const CourseId = async ({ params }: {params: { courseId: string}}) => {
     const course = await db.course.findUnique({
         where: {
             id: params.courseId,
+        },
+    });
+
+    const categories = await db.category.findMany({
+        orderBy:{
+            name: "asc",
         },
     });
 
@@ -68,6 +75,15 @@ const CourseId = async ({ params }: {params: { courseId: string}}) => {
                     initialData={course}
                     courseId={course.id}
                 />
+
+                <CategoryForm 
+                    initialData={course}
+                    courseId={course.id}
+                    options={categories.map((category)=>({
+                        label: category.name,
+                        value: category.id,
+                    }))}
+                />  
 
             </div>
 
---
+++ scripts/seed.ts
@@ -0,0 +1,55 @@
+const { PrismaClient } = require('@prisma/client');
+
+const database = new PrismaClient();
+
+async function main() {
+    try {
+        await database.category.createMany({
+            data: [
+                { name: "Comupter Science" },
+                { name: "Mathematics" },
+                { name: "Physics" },
+                { name: "Chemistry" },
+                { name: "Biology" },
+                { name: "Geography" },
+                { name: "History" },
+                { name: "Economics" },
+                { name: "Literature" },
+                { name: "Philosophy" },
+                { name: "Psychology" },
+                { name: "Sociology" },
+                { name: "Law" },
+                { name: "Business" },
+                { name: "Accounting" },
+                { name: "Marketing" },
+                { name: "Finance" },
+                { name: "Management" },
+                { name: "Engineering" },
+                { name: "Architecture" },
+                { name: "Medicine" },
+                { name: "Nursing" },
+                { name: "Pharmacy" },
+                { name: "Dentistry" },
+                { name: "Agriculture" },
+                { name: "Food Science" },
+                { name: "Nutrition" },
+                { name: "Veterinary Medicine" },
+                { name: "Education" },
+                { name: "Languages" },
+                { name: "Art" },
+                { name: "Music" },
+                { name: "Sports" },
+                { name: "Other" },
+            ]
+        });
+
+        console.log("Seeding finished.");
+        
+    } catch (error) {
+        console.log(" Error seeding the database categories", error);
+    } finally{
+        await database.$disconnect();
+    }
+}
+
+main();

456ea8473ada383ee3d05d4a0da7aee4afc5105a
timons-AI <123802219+timons-AI@users.noreply.github.com>
2023-09-24 15:40:16 +0300

    UploadThing & Image Form

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -5,6 +5,7 @@ import { LayoutDashboard } from "lucide-react";
 import { redirect } from "next/navigation";
 import { TitleForm } from "./_components/title-form";
 import { DescriptionForm } from "./_components/description-form";
+import { ImageForm } from "./_components/image-form";
 
 const CourseId = async ({ params }: {params: { courseId: string}}) => {
     const { userId } = auth();
@@ -60,6 +61,10 @@ const CourseId = async ({ params }: {params: { courseId: string}}) => {
                 />
 
                 <DescriptionForm 
+                    initialData={course}
+                    courseId={course.id}
+                />
+                <ImageForm 
                     initialData={course}
                     courseId={course.id}
                 />

cfd863e0b0c5a7ba70b6b6d1822374a312ded41b
timons-AI <123802219+timons-AI@users.noreply.github.com>
2023-10-22 02:04:44 +0300

    courses data table

---
+++ app/api/courses/[courseId]/publish/route.ts
@@ -0,0 +1,57 @@
+import { NextResponse } from "next/server";
+import { auth } from "@clerk/nextjs";
+import { db } from "@/lib/db";
+export async function PATCH(
+    req: Request,
+    { params } : { params: { courseId: string } }
+){
+    try {
+
+        const { userId } = auth();
+        const { courseId } = params;
+
+        if (!userId) {
+            return new NextResponse("Unauthorized", { status: 401 });
+        }
+
+        const course = await db.course.findUnique({
+            where: {
+                id: courseId,
+                userId: userId
+            },
+            include: {
+                chapters: {
+                    include: {
+                        muxData: true,
+                    }
+                }
+            }
+        });
+
+        if (!course) {
+            return new NextResponse("Not Found", { status: 404 });
+        }
+
+        const hasPublishedChapter = course.chapters.some((chapter) => chapter.isPublished);
+
+        if (!course.title || !hasPublishedChapter || !course.description || !course.imageUrl  || !course.price) { 
+            return new NextResponse("Missing requried Fields", { status: 401 });
+        }
+
+        const publishedCourse = await db.course.update({
+            where: {
+                id: courseId,
+                userId
+            },
+            data: {
+                isPublished: true,
+            }
+        });
+
+        return NextResponse.json(publishedCourse);
+        
+    } catch (error) {
+        console.log("[COURSE_ID_PUBLISH]", error);
+        return new NextResponse("Internal Error", { status: 500 });
+    }
+}
---
+++ app/api/courses/[courseId]/unpublish/route.ts
@@ -0,0 +1,45 @@
+import { NextResponse } from "next/server";
+import { auth } from "@clerk/nextjs";
+import { db } from "@/lib/db";
+export async function PATCH(
+    req: Request,
+    { params } : { params: { courseId: string } }
+){
+    try {
+
+        const { userId } = auth();
+        const { courseId } = params;
+
+        if (!userId) {
+            return new NextResponse("Unauthorized", { status: 401 });
+        }
+
+        const course = await db.course.findUnique({
+            where: {
+                id: courseId,
+                userId: userId
+            },
+           
+        });
+
+        if (!course) {
+            return new NextResponse("Not Found", { status: 404 });
+        }
+
+        const unpublishedCourse = await db.course.update({
+            where: {
+                id: courseId,
+                userId
+            },
+            data: {
+                isPublished: false,
+            }
+        });
+
+        return NextResponse.json(unpublishedCourse);
+        
+    } catch (error) {
+        console.log("[COURSE_ID_UNPUBLISH]", error);
+        return new NextResponse("Internal Error", { status: 500 });
+    }
+}

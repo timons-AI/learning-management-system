db382be52aabff7b67694104459dfa97e18300f5
timons-AI <123802219+timons-AI@users.noreply.github.com>
2023-10-23 10:39:22 +0300

    search title

--- app/(dashboard)/(routes)/search/page.tsx
+++ app/(dashboard)/(routes)/search/page.tsx
@@ -1,5 +1,6 @@
 import { db } from "@/lib/db";
 import { Categories } from "./_components/categories";
+import { SearchInput } from "@/components/search-input";
 
 const SearchPage = async () => {
     const categories = await db.category.findMany({
@@ -9,12 +10,17 @@ const SearchPage = async () => {
     });
 
     return ( 
+        <>
+        <div className=" px-6 md:hidden md:mb-0 block py-6" >
+            <SearchInput/>
+        </div>
+       
     <div className=" p-6">
         <Categories
             items = {categories}
             />
-      
-    </div> );
+    </div> 
+    </> );
 }
  
 export default SearchPage;

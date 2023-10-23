f143b1868815e0aec331bf684435faa87b4e375f
timons-AI <123802219+timons-AI@users.noreply.github.com>
2023-10-23 11:45:55 +0300

    Individual courses on search page

--- app/(dashboard)/(routes)/search/page.tsx
+++ app/(dashboard)/(routes)/search/page.tsx
@@ -1,13 +1,41 @@
 import { db } from "@/lib/db";
 import { Categories } from "./_components/categories";
 import { SearchInput } from "@/components/search-input";
+import { getCourses } from "@/actions/get-courses";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import { CoursesList } from "@/components/courses-list";
 
-const SearchPage = async () => {
+
+interface SearchPageProps {
+    searchParams: {
+        title: string;
+        categoryId: string;
+    }
+}
+
+const SearchPage = async ({
+    searchParams,
+}: SearchPageProps) => {
+
+    const { userId } = auth();
+
+    if ( !userId ) {
+        return redirect( "/" );
+    }
+
     const categories = await db.category.findMany({
         orderBy:{
             name: "asc",
         },
     });
+
+    const courses = await getCourses(
+        {
+            ...searchParams,
+            userId,
+        }
+    );
 
     return ( 
         <>
@@ -15,11 +43,12 @@ const SearchPage = async () => {
             <SearchInput/>
         </div>
        
-    <div className=" p-6">
+    <div className=" p-6 space-y-4">
         <Categories
             items = {categories}
             />
-    </div> 
+            <CoursesList items={courses}/>
+    </div>
     </> );
 }
  
---
+++ components/course-card.tsx
@@ -0,0 +1,68 @@
+import Image from "next/image";
+import Link from "next/link";
+import { IconBadge } from "@/components/icon-badge";
+import { BookOpen, Divide } from "lucide-react";
+import { formatPrice } from "@/lib/formats";
+
+interface CourseCardProps {
+    id: string;
+    title: string;
+    chaptersLength: number;
+    price: number;
+    progress: number | null;
+    category: string | null;
+    imageUrl: string | null;
+}
+
+export const CourseCard = ({
+    id,
+    title,
+    chaptersLength,
+    price,
+    progress,
+    category,
+    imageUrl,
+}: CourseCardProps) => {
+
+        return (
+            <Link href={`/course/${id}`}>
+                <div className=" group hover:shadow-sm transition overflow-hidden border rounded-lg p-3 h-full">
+                    <div className=" relative w-full aspect-video rounded-md overflow-hidden">
+                        <Image
+                            fill 
+                            className="object-cover"
+                            alt={title}
+                            src={imageUrl!}
+                            />
+                    </div>
+                    <div className=" flex flex-col pt-2">
+                        <div className=" text-lg md:text-base font-medium group-hover:text-sky-700 transition line-clamp-2">
+                            {title}
+                        </div>
+                        <p className=" text-sm text-muted-foreground items-center">
+                            {category}
+                        </p>
+                        <div className=" my-3 flex items-center gap-x-2 text-sm md:text-xs">
+                            <div className=" flex items-center gap-x-1 text-slate-500">
+                                <IconBadge  size="sm" icon={BookOpen}/>
+                                    <span>
+                                        {chaptersLength} {chaptersLength === 1 ? "Chapter" : "Chapters"}
+                                    </span>
+                            </div>
+                        </div>
+                        {progress !== null ? (
+                            <div>
+                                TODO: Progress component 
+                            </div>
+                        ) : (
+                            <p className=" text-md md:text-sm font-medium text-slate-700">
+                                {formatPrice(price)}
+                            </p>
+                        )}
+                    
+                    </div>
+                </div>
+                
+            </Link>
+    )
+}

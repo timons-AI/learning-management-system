361280d898b15f9d6b98af43594c7bab4c97b366
timons-AI <123802219+timons-AI@users.noreply.github.com>
2023-12-17 23:27:34 +0300

    Update logo.tsx

--- app/(dashboard)/_components/logo.tsx
+++ app/(dashboard)/_components/logo.tsx
@@ -4,7 +4,7 @@ export const Logo = () => {
   return (
     <div className="flex items-center justify-center">
       <Image src="/logo.png" width={60} height={60} alt="logo" />
-      <h1 className="text-center mt-5 text-2xl font-bold text-gray-800">
+      <h1 className=" hidden md:block text-center mt-5 text-2xl font-bold text-gray-800">
         OLYMPUS
       </h1>
     </div>

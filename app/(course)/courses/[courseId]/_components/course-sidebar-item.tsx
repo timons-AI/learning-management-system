2cf26fd507e10efa7e5f3e616616e1aa13d9ac05
timons-AI <123802219+timons-AI@users.noreply.github.com>
2023-12-17 21:37:08 +0300

    Update course-sidebar-item.tsx
    more css

--- app/(course)/courses/[courseId]/_components/course-sidebar-item.tsx
+++ app/(course)/courses/[courseId]/_components/course-sidebar-item.tsx
@@ -35,20 +35,20 @@ export const CourseSidebarItem = ({
       onClick={onClick}
       type="button"
       className={cn(
-        "flex items-center gap-x-2 text-slate-500 text-sm font-[500] pl-6 transition-all hover:text-slate-600 hover:bg-slate-300/20 rounded-xl mx-3",
+        "flex items-center gap-x-2 text-slate-500 text-sm font-[500] pl-6 transition-all hover:text-slate-600 hover:bg-slate-300/20 rounded-xl mx-2",
         isActive &&
           "text-slate-700 bg-slate-200/20 hover:bg-slate-200/20 hover:text-slate-700",
-        isCompleted && "text-emerald-700 hover:text-emerald-700",
-        isCompleted && isActive && "bg-emerald-200/20"
+        isCompleted && "text-primary hover:text-primary",
+        isCompleted && isActive && "bg-primary/20"
       )}
     >
-      <div className="flex items-center gap-x-2 py-4">
+      <div className="flex items-center gap-x-2 py-3">
         <Icon
           size={22}
           className={cn(
             "text-slate-500",
             isActive && "text-slate-700",
-            isCompleted && "text-emerald-700"
+            isCompleted && "text-primary"
           )}
         />
         {label}

8c51e3fcf747ee75b6474d0743e073f14eac7c32
timons-AI <123802219+timons-AI@users.noreply.github.com>
2023-12-17 23:29:41 +0300

    Update course-mobile-sidebar.tsx

--- app/(course)/courses/[courseId]/_components/course-mobile-sidebar.tsx
+++ app/(course)/courses/[courseId]/_components/course-mobile-sidebar.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { Menu } from "lucide-react";
 import { Chapter, Course, UserProgress } from "@prisma/client";
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";

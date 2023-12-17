87c26432844051003e4dfe5901c148a00da136e5
timons-AI <123802219+timons-AI@users.noreply.github.com>
2023-12-18 00:15:24 +0300

    logo and theme

--- app/(dashboard)/_components/logo.tsx
+++ app/(dashboard)/_components/logo.tsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 export const Logo = () => {
   return (
     <div className="flex items-center justify-center">
-      <Image src="/logo.png" width={60} height={60} alt="logo" />
-      <h1 className=" hidden md:block text-center mt-5 text-2xl font-bold text-gray-800">
+      <Image src="/logo.svg" width={60} height={60} alt="logo" />
+      <h1 className=" hidden md:block text-center mt-5 text-xl font-bold text-gray-800">
         OLYMPUS
       </h1>
     </div>
